595032b9b7895508177f3cc70bc7ce187bef0d8c
danella_patrick <62329649+danellapatrick@users.noreply.github.com>
2021-05-04 23:29:54 +0500

    Update urls.py
    Adding the URL of demo

--- danCV/CV/urls.py
+++ danCV/CV/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 urlpatterns = [
 
     path('', views.home, name='home'),
+    path('demo', views.demo, name='demo'),
     url(r'^view-pdf/$',views.pdf_view, name='pdf_view'),
 
 ]
